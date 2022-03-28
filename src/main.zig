6b9bd4669350485814ace34f6a80f6c48ce5e9c0
Quentin <49767738+quentin-k@users.noreply.github.com>
2022-03-27 22:05:00 -0500

    Turns out you can have rwx pages...

--- src/main.zig
+++ src/main.zig
@@ -3,48 +3,43 @@ const options = @import("options");
 
 pub fn main() !void {
     const fn_bytes = [_]u8{ 197, 251, 88, 193, 195, 102, 102, 46, 15, 31, 132, 0, 0, 0, 0, 0 };
-    const allocator = if (options.valgrind) std.heap.c_allocator else  std.heap.page_allocator;
+    const allocator = if (options.valgrind) std.heap.c_allocator else std.heap.page_allocator;
     var slice: []align(std.mem.page_size) u8 = try allocator.alignedAlloc(u8, std.mem.page_size, std.mem.page_size);
     defer dynFree(allocator, slice);
+    // defer allocator.free(slice);
     slice[0..fn_bytes.len].* = fn_bytes;
     for (fn_bytes) |b, i| slice[i] = b;
-    try setX(slice);
+    try setRwx(slice);
     var mem_fn = @ptrCast(fn (f64, f64) f64, slice.ptr);
     const n1: f64 = 1.23;
     const n2: f64 = 4.56;
     std.log.info("{d}", .{mem_fn(n1, n2)});
     std.log.info("{*}", .{slice.ptr});
 
-    try setRw(slice);
     slice[2] = 92;
-    try setX(slice);
     std.log.info("{d}", .{mem_fn(n1, n2)});
     std.log.info("{*}", .{slice.ptr});
 
-    try setRw(slice);
     slice[2] = 89;
-    try setX(slice);
     std.log.info("{d}", .{mem_fn(n1, n2)});
     std.log.info("{*}", .{slice.ptr});
 
-    try setRw(slice);
-    slice[2] = 94;
-    try setX(slice);
+    // slice[2] = 94;
     std.log.info("{d}", .{mem_fn(n1, n2)});
     std.log.info("{*}", .{slice.ptr});
 }
 
 fn dynFree(allocator: std.mem.Allocator, slice: []align(std.mem.page_size) u8) void {
-    setRw(slice) catch @panic("😬");
+    setRw(slice);
     allocator.free(slice);
 }
 
-fn setX(slice: []align(std.mem.page_size) u8) !void {
-    try std.os.mprotect(slice, std.os.PROT.EXEC);
+fn setRw(slice: []align(std.mem.page_size) u8) void {
+    std.os.mprotect(slice, std.os.PROT.WRITE | std.os.PROT.READ) catch @panic("😱");
 }
 
-fn setRw(slice: []align(std.mem.page_size) u8) !void {
-    try std.os.mprotect(slice, std.os.PROT.WRITE | std.os.PROT.READ);
+fn setRwx(slice: []align(std.mem.page_size) u8) !void {
+    try std.os.mprotect(slice, std.os.PROT.EXEC | std.os.PROT.WRITE | std.os.PROT.READ);
 }
 
 // Add -> { 197, 251, 88, 193, 195, 102, 102, 46, 15, 31, 132, 0, 0, 0, 0, 0 }
